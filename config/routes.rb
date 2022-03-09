8b731e8d0df9376548dc4f1632f4d058a276257c
Nikola Đuza <nikolaseap@gmail.com>
2022-03-09 14:59:27 +0100

    Load record stats with render_async

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get :record_stats, controller: :records
+
+  resources :records
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
